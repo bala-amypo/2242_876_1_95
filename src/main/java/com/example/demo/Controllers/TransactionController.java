714275e4fbeb5de4d1312f0614fcd26ffacf8cb4
bala-amypo <bala@amypo.in>
2025-12-18 17:19:50 +0000

    User commit

---
+++ src/main/java/com/example/demo/Controllers/TransactionController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+import com.example.demo.model.Category;
+import com.example.demo.service.CategoryService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+    private final CategoryService categoryService;
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+
+    }
+    @PostMapping("/")
+    public Category createCategory(@RequestBody Category category) {
+        return categoryService.addCategory(category);
+        }
+    @GetMapping("/")
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+        }
+   }
