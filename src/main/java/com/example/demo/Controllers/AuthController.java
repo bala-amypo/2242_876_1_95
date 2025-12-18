0814be5f22e005da12ed9ae507a327e460e97193
bala-amypo <bala@amypo.in>
2025-12-18 16:15:25 +0000

    User commit

--- src/main/java/com/example/demo/Controllers/AuthController.java
+++ src/main/java/com/example/demo/Controllers/AuthController.java
@@ -1,48 +1,20 @@
 package com.example.demo.controller;
-
-
-
 import com.example.demo.dto.*;
-
 import com.example.demo.model.User;
-
 import com.example.demo.security.JwtTokenProvider;
-
 import com.example.demo.service.UserService;
-
 import org.springframework.security.authentication.*;
-
 import org.springframework.web.bind.annotation.*;
 
-
-
 @RestController
-
 @RequestMapping("/auth")
-
 public class AuthController {
-
-
-
-        private final UserService userService;
-
-            private final AuthenticationManager authenticationManager;
-
-                private final JwtTokenProvider jwtTokenProvider;
-
-
-
-                    public AuthController(UserService userService,
-                    )
-                                              AuthenticationManager authenticationManager,
-
-                                                                        JwtTokenProvider jwtTokenProvider) {
-
-                                                                                    this.userService = userService;
-
-                                                                                            this.authenticationManager = authenticationManager;
-
-                                                                                                    this.jwtTokenProvider = jwtTokenProvider;
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+        public AuthController(UserService userService, AuthenticationManager authenticationManager,JwtTokenProvider jwtTokenProvider) {
+             this.userService = userService;
+             this.authenticationManager = authenticationManager;                                                                                               this.jwtTokenProvider = jwtTokenProvider;
 
                                                                         }
 
@@ -112,11 +84,33 @@ public class AuthController {
 
 
 
-                                                                                                                                    String token = jwtTokenProvider.generateT
-                                                                                                                                                )
-                                                                                                                    )
+                                                                                                                                    String token = jwtTokenProvider.generateToken(
+
+                                                                                                                                                        authentication,
+
+                                                                                                                                                                        user.getId(),
+
+                                                                                                                                                                                        user.getEmail(),
+
+                                                                                                                                                                                                        user.getRole()
+
+                                                                                                                                    );
+
+
+
+                                                                                                                                            return new AuthResponse(
+
+                                                                                                                                                                token,
+
+                                                                                                                                                                                user.getId(),
+
+                                                                                                                                                                                                user.getEmail(),
+
+                                                                                                                                                                                                                user.getRole()
+
+                                                                                                                                            );
+
                                                                                         }
-                                                                                                                                                                            )
-                                                                                }
-                                                                        }
+
 }
+
