ad107c29b1a02522919906b802a50d0086214b78
bala-amypo <bala@amypo.in>
2025-12-18 17:10:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/Controllers/BudgetPlanController.java
@@ -0,0 +1,62 @@
+package com.example.demo.controller;
+
+
+
+import com.example.demo.model.BudgetPlan;
+
+import com.example.demo.service.BudgetPlanService;
+
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+
+@RequestMapping("/budgets")
+
+public class BudgetPlanController {
+
+
+
+    private final BudgetPlanService budgetPlanService;
+
+
+
+    public BudgetPlanController(BudgetPlanService budgetPlanService) {
+
+        this.budgetPlanService = budgetPlanService;
+
+        }
+
+
+
+        @PostMapping("/{userId}")
+
+        public BudgetPlan createBudgetPlan(@PathVariable Long userId,
+        )
+        @RequestBody BudgetPlan plan) {
+
+            return budgetPlanService.createBudgetPlan(userId, plan);
+
+            }
+
+
+
+            @GetMapping("/{userId}/{month}/{year}")
+
+            public BudgetPlan getBudgetPlan(@PathVariable Long userId,
+            )
+            @PathVariable Integer month,
+
+            @PathVariable Integer year) {
+
+            return budgetPlanService.getBudgetPlan(userId, month, year);
+
+            }
+
+            }
+
+            
+        }
+    }
+}
