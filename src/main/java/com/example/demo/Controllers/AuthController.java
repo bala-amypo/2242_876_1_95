9b1a9f028948945894640aa77bced17298274dec
bala-amypo <bala@amypo.in>
2025-12-18 16:45:25 +0000

    User commit

--- src/main/java/com/example/demo/Controllers/AuthController.java
+++ src/main/java/com/example/demo/Controllers/AuthController.java
@@ -3,131 +3,61 @@ import com.example.demo.dto.*;
 import com.example.demo.model.User; 
 import com.example.demo.security.JwtTokenProvider; 
 import com.example.demo.service.UserService; 
-
 import org.springframework.security.authentication.*; 
-
 import org.springframework.web.bind.annotation.*; 
 
- 
 
 @RestController 
-
 @RequestMapping("/auth") 
-
 public class AuthController { 
-
- 
-
     private final UserService userService; 
-
     private final AuthenticationManager authenticationManager; 
-
     private final JwtTokenProvider jwtTokenProvider; 
-
- 
-
     public AuthController(UserService userService, 
-
                           AuthenticationManager authenticationManager, 
-
                           JwtTokenProvider jwtTokenProvider) { 
 
         this.userService = userService; 
-
         this.authenticationManager = authenticationManager; 
-
         this.jwtTokenProvider = jwtTokenProvider; 
-
-    } 
-
- 
-
+   } 
     @PostMapping("/register") 
-
     public AuthResponse register(@RequestBody RegisterRequest request) { 
-
- 
-
         User user = new User(null, 
-
-                request.getName(), 
-
-                request.getEmail(), 
-
-                request.getPassword(), 
-
-                null); 
-
- 
+            request.getName(), 
+            request.getEmail(), 
+            request.getPassword(), 
+            null); 
 
         User savedUser = userService.register(user); 
-
- 
-
         return new AuthResponse( 
-
-                null, 
-
-                savedUser.getId(), 
-
-                savedUser.getEmail(), 
-
-                savedUser.getRole() 
-
+            null, 
+            savedUser.getId(), 
+            savedUser.getEmail(), 
+            savedUser.getRole() 
         ); 
-
     } 
-
- 
-
-    @PostMapping("/login") 
-
-    public AuthResponse login(@RequestBody LoginRequest request) { 
-
- 
-
-        Authentication authentication = 
-
-                authenticationManager.authenticate( 
-
-                        new UsernamePasswordAuthenticationToken( 
-
-                                request.getEmail(), 
-
-                                request.getPassword() 
-
-                        ) 
-
-                ); 
-
- 
-
+@PostMapping("/login") 
+public AuthResponse login(@RequestBody LoginRequest request) { 
+    Authentication authentication = 
+        authenticationManager.authenticate( 
+            new UsernamePasswordAuthenticationToken(
+                request.getEmail(), 
+                request.getPassword() 
+                ) 
+            ); 
         User user = userService.findByEmail(request.getEmail()); 
-
- 
-
         String token = jwtTokenProvider.generateToken( 
-
                 authentication, 
-
                 user.getId(), 
-
                 user.getEmail(), 
-
                 user.getRole() 
-
         ); 
 
- 
-
-        return new AuthResponse( 
-
+    return new AuthResponse( 
                 token, 
-
                 user.getId(), 
-
                 user.getEmail(), 
-
                 user.getRole() 
 
         ); 
