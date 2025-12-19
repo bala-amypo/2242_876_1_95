ec027d22ef310bebca120f63b73d23c23e96846e
bala-amypo <bala@amypo.in>
2025-12-19 07:48:16 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,26 +1,28 @@
 package com.example.demo.controller;
+
 import com.example.demo.model.Category;
 import com.example.demo.service.CategoryService;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
+
     private final CategoryService categoryService;
-        public CategoryController(CategoryService categoryService) {
-                    this.categoryService = categoryService;
-                              }
-                              @PostMapping("/")
-                              public Category createCategory(@RequestBody Category category) {
-                                  return categoryService.addCategory(category);
-                                      }
-                                      @GetMapping("/")
-                                      public List<Category> getAllCategories() {
-                                              return categoryService.getAllCategories();
-                                                      }   
-                                                              
-                                                               }
 
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
-                                                               
+    @PostMapping
+    public Category create(@RequestBody Category category) {
+        return categoryService.addCategory(category);
+    }
+
+    @GetMapping
+    public List<Category> list() {
+        return categoryService.getAllCategories();
+    }
+}
