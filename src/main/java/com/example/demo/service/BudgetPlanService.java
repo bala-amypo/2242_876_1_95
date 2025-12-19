5acfcc33a113b0a0d6276dace867a5934dfdbeb0
bala-amypo <bala@amypo.in>
2025-12-19 09:49:07 +0000

    User commit

--- src/main/java/com/example/demo/service/BudgetPlanService.java
+++ src/main/java/com/example/demo/service/BudgetPlanService.java
@@ -1,6 +1,5 @@
 package com.example.demo.service;
 
-import com.example.demo.model.BudgetPlan;
 
 public interface BudgetPlanService {
     BudgetPlan createBudgetPlan(Long userId, BudgetPlan plan);
--- src/main/java/com/example/demo/service/TransactionService.java
+++ src/main/java/com/example/demo/service/TransactionService.java
@@ -1,6 +1,6 @@
 package com.example.demo.service;
 
-import com.example.demo.model.TransactionLog;
+//import com.example.demo.model.TransactionLog;
 import java.util.List;
 
 public interface TransactionService {
