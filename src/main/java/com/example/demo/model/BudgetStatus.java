09fe5aa18cbafc6e50b55e4dcef001aa30c45212
bala-amypo <bala@amypo.in>
2025-12-19 09:31:07 +0000

    User commit

--- src/main/java/com/example/demo/model/BudgetStatus.java
+++ src/main/java/com/example/demo/model/BudgetStatus.java
@@ -1,6 +1,11 @@
-// package com.example.demo.model;
+package com.example.demo.model;
 
-// public enum BudgetStatus {
-//     UNDER_LIMIT,
-//     OVER_LIMIT
-// }
+public final class BudgetStatus {
+
+    public static final String UNDER_LIMIT = "UNDER_LIMIT";
+    public static final String OVER_LIMIT = "OVER_LIMIT";
+
+    // Private constructor to prevent object creation
+    private BudgetStatus() {
+    }
+}
