ccc103e26e6efd799e102646130bc0f263844942
bala-amypo <bala@amypo.in>
2025-12-19 08:00:16 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/BudgetSummaryRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.BudgetPlan;
+import com.example.demo.model.BudgetSummary;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BudgetSummaryRepository extends JpaRepository<BudgetSummary, Long> {
+    Optional<BudgetSummary> findByBudgetPlan(BudgetPlan plan);
+}
