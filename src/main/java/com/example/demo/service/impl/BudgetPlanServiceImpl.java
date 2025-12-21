25bb7879519054c93f122e44396e70b2806e81e3
bala-amypo <bala@amypo.in>
2025-12-21 11:45:28 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/BudgetPlanServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BudgetPlanServiceImpl.java
@@ -2,9 +2,9 @@ package com.example.demo.service.impl;
 
 import com.example.demo.exception.BadRequestException;
 import com.example.demo.model.BudgetPlan;
-//import com.example.demo.model.User;
+import com.example.demo.model.User;
 import com.example.demo.repository.BudgetPlanRepository;
-//import com.example.demo.repository.UserRepository;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.service.BudgetPlanService;
 import org.springframework.stereotype.Service;
 
