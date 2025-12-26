d5c92080f43f3f30f4c928f227299df140d71287
bala-amypo <bala@amypo.in>
2025-12-26 10:14:58 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,1 +1,47 @@
-c
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        Server localServer = new Server()
+                .url("https://9132.32procr.amypo.ai/")
+                .description("Local Server");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Personal Finance Budget Planner API")
+                        .description("API documentation for Personal Finance Budget Planner")
+                        .version("v1")
+                )
+                .externalDocs(new ExternalDocumentation()
+                        .description("Project Documentation")
+                )
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearerAuth")
+                )
+                .components(
+                        new Components().addSecuritySchemes("bearerAuth", bearerAuth)
+                )
+                .servers(List.of(localServer));
+    }
+}
